b8cf7e6411befa5bec3a72eb559c94e2701436bb
Super-WeiDong <weidongwang@brandeis.edu>
2023-06-25 15:57:14 -0400

    Ribbon

--- search/src/main/java/com/example/search/SearchApplication.java
+++ search/src/main/java/com/example/search/SearchApplication.java
@@ -3,12 +3,16 @@ package com.example.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableDiscoveryClient
+@RibbonClient(name = "search")
 public class SearchApplication {
 
     public static void main(String[] args) {
